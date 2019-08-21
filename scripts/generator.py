5923dbbc88faf2e5aba42caea7de5539f85c1459
atsukoba <t16366ak@sfc.keio.ac.jp>
2019-08-22 02:58:52 +0900

    implement type model selection

--- scripts/generator.py
+++ scripts/generator.py
@@ -1,42 +1,27 @@
-import os
-import json
-import argparse
-import logging
 import numpy as np
 import tensorflow as tf
+from scripts import model_loader
 from logging import getLogger
 from datetime import datetime as d
-from pythonosc import osc_message_builder, udp_client
-from pythonosc import dispatcher, osc_server
-from scipy.io.wavfile import write as sound_write
 
 
 class Generator:
 
-    def __init__(self):
-        return
+    def __init__(self, model_type="birds", useall=True):
+        self.models = model_loader.all() if useall else model_loader.load(model_type)
+        self.type = model_type
 
-    def 
+    def gen(self, input_z) -> "numpy.ndarray":
+        
+        v = self.models[self.type]["sess"].run(
+            self.models[self.type]["G_z"],
+            {self.models[self.type]["z"]: input_z})[0, :, 0]
+        return v
 
+    def set_type(self, model_type: str) -> None:
+        assert model_type in ["birds", "drums", "piano", "sc09", "timit"],\
+            f"invalid model type name {model_type}"
+        self.type = model_type
 
-def generate(*value) -> None:
-    # Generator input variable `z`
-    log("start generating")
-    # cut value 256 -> 100
-    input_z = value[1:101]
-    assert len(input_z) == 100, f"invalid input z shape, expected dim is 100 != {len(input_z)}"
-    input_z = np.array(list(map(float, input_z))).reshape(1, 100)
-
-    generated = sess.run(G_z, {z: input_z})[0, :, 0]
-    path = "sounds/{}.wav".format(d.now().strftime('%Y%m%d-%H%M%S'))
-    log(f"saving .wav file...")
-    sound_write(path, 16000, generated)
-    log(f"saved: {path}")
-    _osc_send_msg(conf["address"], os.getcwd() + "/" + path)
-
-    # .wav file
-    global saved_data_pathes
-    saved_data_pathes.append(path)
-    if os.path.exists(saved_data_pathes[0]):
-        os.remove(saved_data_pathes[0])
-    saved_data_pathes.pop(0)
+if __name__ == "__main__":
+    pass
